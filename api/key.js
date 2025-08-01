67827ca98b383f918dafbf1727b09763b3c5c888
Faisal Rehman Ansari <21bsai47@quest.edu.pk>
2025-08-01 19:27:09 +0500

    Update key.js

--- api/key.js
+++ api/key.js
@@ -1,4 +1,4 @@
 // /api/key.js
 export default function handler(req, res) {
-  res.status(200).json({ key: process.env.OPENROUTER_API_KEY });
+  res.status(200).json({ key: process.env.API_KEY });
 }
